b892998b96fefb4d0262f68e7b005827c7e0004f
filippo-bilardo <filippo.bilardo@tiscali.it>
2024-03-11 11:07:23 +0000

    update

---
+++ Menu/progr_show.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+source progr.sh
+
+tasks_in_total=37
+for current_task in $(seq $tasks_in_total) 
+    do
+    sleep 0.2 #simulate the task running
+    show_progress $current_task $tasks_in_total
+done
