8c97b73b0fc96b977069ff2d12f4b2d495b3c637
filippo-bilardo <filippo.bilardo@tiscali.it>
2024-05-08 10:45:14 +0000

    update

---
+++ ES02/variab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cont=10
+
+echo "Il valore della variabile cont è: $cont"
+echo 'Il valore della variabile cont è: $cont'
+
+echo -e "\n \t Il valore della variabile \$cont è: $cont"
+
